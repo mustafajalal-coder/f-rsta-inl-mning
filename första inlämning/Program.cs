1a53dea819df5891c1dcc3538f01d2024d94fc84
muja07001 <muja07001@E-PW05CGR9>
2024-09-12 14:32:13 +0200

    Add project files.

---
+++ första inlämning/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace första_inlämning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            //kommentar: det skriver ut text.
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\");
+            //kommmentar: \ tas bort om den är framför ett tecken. 
+
+
+        }
+    }
+}
